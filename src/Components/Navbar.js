1cb6fd5afd40f1ca5f1992a1e96871e669ab89e2
Melyn Atieno Were <melynatieno@gmail.com>
2023-02-21 20:41:04 +0300

    style navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import "../Styles/Navbar.css"
+import { Link } from "react-router-dom"
 
 function Navbar() {
   return (
     <div>
-        <nav>
-            <h1><b>PETSY</b></h1>
-            <ul>
-
-                <li><a href='home'><b>HOME</b></a></li>
-                <li><a href='whyus'><b>WHY US?</b></a></li>
-                <li><a href='contact'><b>CONTACT</b></a></li>
-            </ul>
+        <nav className='home-nav'>
+            <h1 className='petsy-nav'><b>PETSY</b></h1>
+            
+                <Link to='/home'className='nav-item'><b>HOME</b></Link>
+                <Link to='/whyus?' className='nav-item'><b>WHY US?</b></Link>
+                <Link to='/contact' className='nav-item'><b>CONTACT</b></Link>
+          
         </nav>
     </div>
   )
