1c3ced62959360ef4e4c42824877dcf5ce93bd56
Melyn Atieno Were <melynatieno@gmail.com>
2023-02-21 21:16:51 +0300

    add shop now button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import "../Styles/Home.css"
+import TypewriterComponent from 'typewriter-effect'
 
 function Home() {
   return (
-    <div>
+    <div className='homepage'>
+        <div className='header'>
+                <h1 className='needpet'><b>NEED A PET?</b></h1>
+                <h1 className='gotcha'> <TypewriterComponent 
+          options={{
+            strings:["WE GOT YOU !"],
+            autoStart: true,
+            loop: true,
+            pauseFor:50
+          }}
+        />
+        </h1>
+        </div>
+    
+    <div className='shop-now-button'>
         <button className='shop-now'><b>SHOP NOW</b></button>
+    </div>
     </div>
   )
 }
