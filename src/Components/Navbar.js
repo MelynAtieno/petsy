e93a63de1f06c4b3780a65b47a9bd92938a3d8c6
Melyn Atieno Were <melynatieno@gmail.com>
2023-02-22 18:41:02 +0300

    change text color navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div>
         <nav className='home-nav'>
-            <h1 className='petsy-nav'><b>PETSY</b></h1>
+            <Link to="/home" className='petsy-nav'><b>PETSY</b></Link>
             
                 <Link to='/home'className='nav-item'><b>HOME</b></Link>
                 <Link to='/whyus?' className='nav-item'><b>WHY US?</b></Link>
