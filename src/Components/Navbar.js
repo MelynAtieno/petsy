4936179a58ef7e6db38a3bd078100cb7519c0c12
Melyn Atieno Were <melynatieno@gmail.com>
2023-05-01 11:38:52 +0300

    create whyus page

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,14 +5,13 @@ import { Link } from "react-router-dom"
 function Navbar() {
   return (
     <div>
-        <nav className='home-nav'>
-            <Link to="/home" className='petsy-nav'><b>PETSY</b></Link>
-            
-                <Link to='/home'className='nav-item'><b>HOME</b></Link>
-                <Link to='/whyus?' className='nav-item'><b>WHY US?</b></Link>
-                <Link to='/contact' className='nav-item'><b>CONTACT</b></Link>
-          
-        </nav>
+      <nav className='home-nav'>
+        <Link to="/home" className='petsy-nav'><b>PETSY</b></Link>
+        <Link to='/home' className='nav-item'><b>HOME</b></Link>
+        <Link to='/whyus?' className='nav-item'><b>WHY US?</b></Link>
+        <Link to='/contact' className='nav-item'><b>CONTACT</b></Link>
+
+      </nav>
     </div>
   )
 }
