4b4b1d858a8f0d084357130f05da576f55884a04
Melyn Atieno Were <melynatieno@gmail.com>
2023-05-01 12:43:57 +0300

    add routes and contact page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import {Route, Routes} from "react-router-dom";
+import React from 'react';
+import Whyus from "./Components/Whyus";
 
 
 function App() {
@@ -9,6 +12,12 @@ function App() {
       
       <Navbar />
       <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/whyus' element={<Whyus />}/>
+        <Route path='/Contact' element={<Contact />}/>
+      </Routes>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,13 +8,12 @@ function Navbar() {
     <div>
       
       <nav className='home-nav'>
+      <a href='/home' className='petsy-nav'><b>PETSY</b></a>
         <ul>
-        <li>
-        <a href='/home' className='petsy-nav'><b>PETSY</b></a>
-        <a href='/home' className='nav-item'><b>HOME</b></a>
-        <a href='/whyus?' className='nav-item'><b>WHY US?</b></a>
-        <a href='/contact' className='nav-item'><b>CONTACT</b></a>
-        </li>
+        
+        <li><a href='/home' className='nav-item'><b>HOME</b></a></li>
+        <li><a href='/whyus' className='nav-item'><b>WHY US?</b></a></li>
+        <li><a href='/contact' className='nav-item'><b>CONTACT</b></a></li>
         </ul>
       </nav>
       
