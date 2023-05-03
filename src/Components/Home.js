329f8c6ebf777632cbb22a718226c0c7ba761722
Melyn Atieno Were <melynatieno@gmail.com>
2023-05-03 18:19:11 +0300

    remove the 5 pet images from home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,7 @@
 import React from 'react'
 import "../Styles/Home.css"
 import TypewriterComponent from 'typewriter-effect'
-import cat1 from "../Images/cat1.jpg"
-import dog2 from "../Images/dog2.jpg"
-import parrot1 from "../Images/parrot1.jpg"
-import rabbit1 from "../Images/rabbit1.jpg"
-import dog3 from "../Images/dog3.jpg"
+
 
 function Home() {
   return (
@@ -23,30 +19,7 @@ function Home() {
         </h1>
         </div>
 
-          <div className='img-top'>
-        <div className='img-top-column'>
-            <img className='cat1' src={cat1} alt='cat1'></img>
-        </div>
-
-        <div className='img-top-column'> 
-            <img className='dog3' src={dog3} alt="dog3"></img>
-        </div>
-        </div>
-        <div className='img-row'>
-
-        <div className='img-column'>
-          <img className='dog2' src={dog2} alt="dog2"></img>
-        </div>
-
-        <div className='img-column'>
-          <img className='parrot1' src={parrot1} alt="parrot1"></img>
-        </div>
-
-    <div className='img-column'>
-          <img className='rabbit1' src={rabbit1} alt="rabbit"></img>
-    </div>
-
-        </div>
+          
    
     <div className='shop-now-button'>
         <button className='shop-now'><b>SHOP NOW</b></button>
