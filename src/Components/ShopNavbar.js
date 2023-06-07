aff6ed2ec2f6c743dba5cdf5242ce2cadcd7bcb5
Melyn Atieno Were <melynatieno@gmail.com>
2023-06-07 19:21:57 +0300

    add sidenavbar js file

---
+++ src/Components/ShopNavbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function ShopNavbar() {
+  return (
+    <div className='sidenav-container'>
+        <div className='sidenav'>
+
+        </div>
+    </div>
+  )
+}
+
+export default ShopNavbar
