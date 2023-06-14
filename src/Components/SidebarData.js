eb5f29a14082ba5a7ea44668594d11874229a784
Melyn Atieno Were <melynatieno@gmail.com>
2023-06-14 20:43:06 +0300

    add sidebar data

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { FaHome, FaCat, FaDog } from "react-icons/fa"
+import {  GiRabbit, GiParrotHead} from "react-icons/gi"
+
+export const SidebarData = [
+    {
+        title: "HOME",
+        icon: <FaHome />,
+        link: "/home"
+    },
+    {
+        title: "CATS",
+        icon: <FaCat />,
+        link: "/shop-cats"
+    },
+    {
+        title: "DOGS",
+        icon: <FaDog />,
+        link: "/shop-dogs"
+    },
+    {
+        title: "RABBITS",
+        icon: <GiRabbit />,
+        link: "/shop-rabbits"
+    },
+    {
+        title: "PARROTS",
+        icon: <GiParrotHead />,
+        link: "/shop-rabbits"
+    }
+    
+]
