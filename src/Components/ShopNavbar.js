25f6b628780e9fcdec77fb522162c5bf0bd5de41
Melyn Atieno Were <melynatieno@gmail.com>
2023-06-14 21:06:20 +0300

    import sidebar data to shopnavbar and map data

--- src/Components/ShopNavbar.js
+++ src/Components/ShopNavbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import "../Styles/ShopNavbar.css"
+import { SidebarData } from './SidebarData'
 
 function ShopNavbar() {
   return (
     <div className='sidenav-container'>
         <div className='sidenav'>
-               
+            {SidebarData.map((val,key))}   
         </div>
     </div>
   )
