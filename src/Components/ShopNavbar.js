59d2e90f53f9ac00bfd46d47ab05ddd2832da1a9
Melyn Atieno Were <melynatieno@gmail.com>
2023-06-19 20:45:38 +0300

    change sidenav icons to links

--- src/Components/ShopNavbar.js
+++ src/Components/ShopNavbar.js
@@ -19,11 +19,30 @@ function ShopNavbar() {
               )
             })}    */}
             
-            <li  className='sidenav-items'><FaHome size={35}/> <Link to="/shop" className='sidenav-title'><b>HOME</b></Link></li>
-            <li  className='sidenav-items'><FaCat size={35}/> <Link to="/shop-cats" className='sidenav-title'><b>CATS</b></Link></li>
-            <li  className='sidenav-items'><FaDog size={35}/> <Link to="/shop-dogs" className='sidenav-title'><b>DOGS</b></Link></li>
-            <li  className='sidenav-items'><GiRabbit size={35}/> <Link to="/shop-rabbits" className='sidenav-title'><b>RABBITS</b></Link></li>
-            <li  className='sidenav-items'><GiParrotHead size={35}/> <Link to="/shop-parrots" className='sidenav-title'><b>PARROTS</b></Link></li>
+            <li  className='sidenav-items'>
+              <Link to="/shop" className='icon-link'><FaHome size={35}/></Link>
+               <Link to="/shop" className='sidenav-title'><b>HOME</b></Link>
+            </li>
+
+            <li  className='sidenav-items'>
+              <Link to="/shop-cats" className='icon-link'> <FaCat size={35}/></Link> 
+              <Link to="/shop-cats" className='sidenav-title'><b>CATS</b></Link>
+            </li>
+
+            <li  className='sidenav-items'>
+              <Link to="/shop-dogs" className='icon-link'><FaDog size={35}/></Link>
+               <Link to="/shop-dogs" className='sidenav-title'><b>DOGS</b></Link>
+            </li>
+
+            <li  className='sidenav-items'>
+              <Link to="/shop-rabbits" className='icon-link'><GiRabbit size={35}/></Link> 
+              <Link to="/shop-rabbits" className='sidenav-title'><b>RABBITS</b></Link>
+            </li>
+
+            <li  className='sidenav-items'>
+              <Link to="/shop-parrots" className='icon-link'><GiParrotHead size={35}/></Link> 
+              <Link to="/shop-parrots" className='sidenav-title'><b>PARROTS</b></Link>
+            </li>
 
             </ul> 
         </div>
